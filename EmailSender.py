de843b22df18bdfea06662d090150c02fb485d5e
ISLAM-XGAMER <islmhmdymhmed@gmail.com>
2022-04-09 01:22:14 +0300

    First Version

---
+++ EmailSender.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.message import EmailMessage
+
+def userinfo(sender_email2 , receiver_email2 , password2):
+        
+        
+    global sender_email , receiver_email , password
+        
+    sender_email =  sender_email2
+    receiver_email = receiver_email2
+    password = password2
+
+
+
+
+def send(portnum , ip , status):
+
+        
+           
+    message = EmailMessage()
+    if status == "o":
+        message.set_content(f'The port {portnum} is open for the ip {ip}')
+    else:
+        message.set_content(f'The port {portnum} is closed for the ip {ip}')
+        
+    message['Subject'] = 'Port Scanner'
+    message['From'] = sender_email
+    message['To'] = receiver_email
+
+
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login(sender_email, password)
+    server.send_message(message)
+    server.quit()
+
---
+++ Port2Email.py
@@ -0,0 +1,130 @@
+import socket
+import pyfiglet
+import time
+from EmailSender import *
+from getpass import getpass
+
+
+# Now only at the closed most common ports
+
+n = 20
+
+
+
+print(pyfiglet.figlet_format('Port2Email'))
+
+print("""programmed by : islam.h7""")
+
+# s for sender , r for receiver , p for password
+s = input("write your Sender Email  (the email you will send from) : ".title()).replace(" ","")
+r = input("write your receiver Email  (the email you will receive the email at) : ".title()).replace(" ","")
+p =  getpass("write your Sender Email password : ".title()).replace(" ","")
+
+userinfo(s, r, p)
+
+
+
+
+
+
+
+
+
+
+print("""Choose what type of scanning you would like to use:
+
+1 - typed port 
+2 - most common ports
+3 - all ports
+4 - help
+5 - contact with me  """.title())
+
+answer = input('write here : '.title())
+
+
+def all_ports():
+
+    target = input('Type The IP : ')
+    for port in range(1, 65536):
+
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+            r = s.connect_ex((target, port))
+            service = socket.getservbyport(port)
+            if r == 0:
+                print(
+                    f'port {port} is open !!!!!!!! ,  and service is {service}'
+                )
+                send(port , target , "o")
+            else:
+                print(f'port {port} is closed !!!! and service is {service}')
+                send(port , target , "c")
+            s.close()
+        except:
+            pass
+
+
+def typed_port():
+    target = input('Type The IP : ')
+    port = int(input('Type The Port : '))
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        r = s.connect_ex((target, port))
+
+        if r == 0:
+            service = socket.getservbyport(port)
+            print(f'{port} is open !!!!!!!! ,  and service is {service}')
+            send(port , target , "o")
+        else:
+            service = socket.getservbyport(port)
+            print(f'{port} is closed !!!! and service is {service}')
+            s.close()
+            send(port , target , "c")
+    except:
+        print('this port is not found in our system'.title())
+        typed_port()
+
+
+def most_common_ports():
+    target = input('type the IP : ')
+    ports = [21, 20, 19, 22, 23, 443, 80]
+    for port in ports:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        r = s.connect_ex((target, port))
+
+        if r == 0:
+            service = socket.getservbyport(port)
+            print(f'{port} is open !!!!!!!! ,  and service is {service}')
+            send(port , target , "o") 
+        
+        else:
+            service = socket.getservbyport(port)
+            print(f'{port} is closed !!!! and service is {service}')
+            send(port , target , "c") 
+            
+            s.close()
+
+
+if answer == '1':
+    typed_port()
+
+elif answer == '2':
+    most_common_ports()
+
+elif answer == '3':
+    all_ports()
+
+if answer == '4':
+    print("""1 - Typed Port ======> a port chosen by the user 
+2 -  Most Common Ports  ==========> checking most common ports  [21 , 20 , 19 , 22 ,23 , 443 , 80 ]
+3 - All Ports =========> checking all 65,535 port , this going to take some time could be about 1 ~ 1.5 hour """
+          .title())
+
+if answer == '5':
+    print(""" Gmail : islmhmdymhmed@gmail.com
+    this is the only way you can contact with me :) 
+    please read the ReadMe.txt file to know more about this program and ensure it is working correctly :)""")
